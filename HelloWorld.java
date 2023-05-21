d878171014532f8c13aea972fe111d36a340e7df
HazazelNut <dire.serenity@gmail.com>
2023-05-21 19:20:52 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,5 @@
-package helloWorld;
-
-public class HelloWorld {
-
+public class demo{
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 	}
-
 }
