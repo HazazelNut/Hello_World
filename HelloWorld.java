ba40bf8557356d2eaa826373e05c2b5a373908d2
HazazelNut <125711576+HazazelNut@users.noreply.github.com>
2023-05-22 11:59:29 -0400

    Update HelloWorld.java
    Add comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,8 @@
+/*	Selena Phan
+	User: HazazelNut
+	https://github.com/HazazelNut/Hello_World.git
+	CPSC 2150 Lab #0
+*/
 public class demo{
 	public static void main(String[] args) {
 		System.out.println("Hello world");
